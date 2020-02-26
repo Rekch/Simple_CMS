8e56adaab9b11c98b2e9f7afc1cad6357dff754c
Rekch <descale@live.fr>
2020-02-26 14:21:12 -0500

    Respect de PEP8, finalisation des fonctionnalites

--- form.py
+++ form.py
@@ -1,26 +1,81 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField
-from wtforms.validators import DataRequired, Length
+from wtforms.validators import DataRequired, Length, Regexp
 from wtforms.fields.html5 import DateField
 
+
 class PostForm(FlaskForm):
-	title = StringField('Titre', validators=[DataRequired()])
+    title = StringField(
+        'Titre',
+        validators=[
+            DataRequired(),
+            Length(
+                min=2,
+                max=100,
+                message='La longueur minimum est 2 carateres, '
+                'et le maximum est 100 caracteres.')])
 
-	content = TextAreaField('Contenu', validators=[DataRequired()])
+    content = TextAreaField(
+        'Contenu',
+        validators=[
+            DataRequired(),
+            Length(
+                min=2,
+                max=500,
+                message='La longueur minimum est 2 carateres, '
+                'et le maximum est 500 caracteres.')])
 
-	date = DateField('Date parution', format='%Y-%m-%d', validators=[DataRequired()])
+    date = StringField(
+        'Date parution',
+        validators=[
+            DataRequired(),
+            Regexp(
+                r'^(?:[1-9]\d{3}-(?:(?:0[1-9]|1[0-2])-(?:0[1-9]|1\d|2[0-8])|(?:0[13-9]|1[0-2])-(?:29|30)|(?:0[13578]|1[02])-31)|(?:[1-9]\d(?:0[48]|[2468][048]|[13579][26])|(?:[2468][048]|[13579][26])00)-02-29) (?:[01]\d|2[0-3]):[0-5]\d:[0-5]\d\.[0-9]\d{2}$',
+                message="Date invalide, "
+                "le format est YYYY-MM-JJ HH:mm:ss.sss")])
 
-	auteur = StringField('Auteur', validators=[DataRequired(), Length(min=2, max=20)] )
+    auteur = StringField(
+        'Auteur',
+        validators=[
+            DataRequired(),
+            Length(
+                min=2,
+                max=100,
+                message='La longueur minimum est 2 carateres, '
+                'et le maximum est 100 caracteres.')])
 
-	identifiant = StringField('Identifiant', validators=[DataRequired(), Length(min=2, max=20)])
+    identifiant = StringField(
+        'Identifiant', validators=[
+            DataRequired(), Length(
+                min=2, max=50, message='La longueur minimum est 2 carateres, '
+                'et le maximum est 100 caracteres.'),
+            Regexp(
+                r'^\w+$',
+                message='Seul les caracteres '
+                'alphanumeriques sont autorises.')])
 
-	submit = SubmitField('Poster')
-
+    submit = SubmitField('Poster')
 
 
 class ModifyPostForm(FlaskForm):
-	title = StringField('Titre', validators=[DataRequired()])
+    title = StringField(
+        'Titre',
+        validators=[
+            DataRequired(),
+            Length(
+                min=2,
+                max=100,
+                message='La longueur minimum est 2 carateres, '
+                'et le maximum est 100 caracteres.')])
 
-	content = TextAreaField('Contenu', validators=[DataRequired()])
+    content = TextAreaField(
+        'Contenu',
+        validators=[
+            DataRequired(),
+            Length(
+                min=2,
+                max=500,
+                message='La longueur minimum est 2 carateres, '
+                'et le maximum est 500 caracteres.')])
 
-	submit = SubmitField('Poster')
+    submit = SubmitField('Poster')
--- tp1.py
+++ tp1.py
@@ -7,123 +7,146 @@ app.config['SECRET_KEY'] = 'a3a9fb269885a66d27c3875e9d3b99bd'
 
 
 try:
-	db = sqlite3.connect(':memory:', check_same_thread=False)
-	print("La connexion est etablie: la database est cree dans la memoire.")
-	cursor = db.cursor()
+    db = sqlite3.connect(':memory:', check_same_thread=False)
+    print("La connexion est etablie: la database est cree dans la memoire.")
+    cursor = db.cursor()
 
-	fh = open('db.sql', 'r')
-	script = fh.read()
-	cursor.executescript(script)
+    fh = open('db.sql', 'r')
+    script = fh.read()
+    cursor.executescript(script)
 
-	print("Script lu")
+    print("Script lu")
 
 except Exception as e:
-	db.rollback()
-	raise e
-
-#finally:
-#	db.close()
+    db.rollback()
+    raise e
 
 
 posts = [{}]
 postsDup = [{}]
 
+
 @app.route("/")
 @app.route("/accueil")
 def accueil():
-	cursor.execute("SELECT * FROM article WHERE date_publication <= date('now') ORDER BY date_publication DESC LIMIT 5 ;")
-	del posts[:]
-	del postsDup[:]
-	while True:
-		row=cursor.fetchone()
-		if row == None:
-			break
-		post= ( row[0], row[1], row[2], row[3], row[4], row[5])
-		if post not in postsDup:
-			posts.append(post)
-			postsDup.append(post)
-	return render_template('accueil.html', posts=posts)
+    cursor.execute(
+        "SELECT * FROM article WHERE date_publication <= date('now')"
+        " ORDER BY date_publication DESC LIMIT 5 ;")
+    del posts[:]
+    del postsDup[:]
+    while True:
+        row = cursor.fetchone()
+        if row is None:
+            break
+        post = (row[0], row[1], row[2], row[3], row[4], row[5])
+        if post not in postsDup:
+            posts.append(post)
+            postsDup.append(post)
+    return render_template('accueil.html', posts=posts)
 
 
 @app.route("/admin")
 def admin():
-	cursor.execute("SELECT * from article;")
-	del posts[:]
-	del postsDup[:]
-	while True:
-		row=cursor.fetchone()
-		if row == None:
-			break
-		post= ( row[0], row[1], row[2], row[3], row[4], row[5])
-		if post not in postsDup:
-			posts.append(post)
-			postsDup.append(post)
-	print(posts)
-	return render_template('admin.html', posts=posts)
+    cursor.execute("SELECT * from article;")
+    del posts[:]
+    del postsDup[:]
+    while True:
+        row = cursor.fetchone()
+        if row is None:
+            break
+        post = (row[0], row[1], row[2], row[3], row[4], row[5])
+        if post not in postsDup:
+            posts.append(post)
+            postsDup.append(post)
+    print(posts)
+    return render_template('admin.html', posts=posts)
+
 
 @app.route("/admin-nouveau", methods=['GET', 'POST'])
 def admin_nouveau():
-	form = PostForm()
-	if form.validate_on_submit():
-		titre=form.title.data
-		identifiant=form.identifiant.data
-		auteur=form.auteur.data
-		date_parution=form.date.data
-		paragraphe=form.content.data
-		params= ( titre, identifiant, auteur, date_parution, paragraphe)
-		cursor.execute("INSERT INTO article(titre, identifiant, auteur, date_publication, paragraphe) VALUES( ?, ?, ?, ?, ?)", params)
-		flash('Post cree avec succes!', 'success')
-		cursor.execute("SELECT * from article;")
-		print(cursor.fetchall())
-		return redirect(url_for('admin'))
-	return render_template('creation_post.html', title='Nouveau poste', form=form)
+    form = PostForm()
+    if form.validate_on_submit():
+        titre = form.title.data
+        identifiant = form.identifiant.data
+        auteur = form.auteur.data
+        date_parution = form.date.data
+        paragraphe = form.content.data
+        params = (titre, identifiant, auteur, date_parution, paragraphe)
+        cursor.execute(
+            "INSERT INTO article(titre, identifiant, auteur, "
+            "date_publication, paragraphe) VALUES( ?, ?, ?, ?, ?)",
+            params)
+        flash('Post cree avec succes!', 'success')
+        cursor.execute("SELECT * from article;")
+        print(cursor.fetchall())
+        return redirect(url_for('admin'))
+    return render_template(
+        'creation_post.html',
+        title='Nouveau poste',
+        form=form)
+
 
 @app.route("/article/<identifiant>")
 def article(identifiant):
-	post=[]
-	cursor.execute("SELECT * FROM article WHERE identifiant = (?);", (identifiant,))
-	while True:
-		row=cursor.fetchone()
-		if row == None:
-			break
-		post= ( row[0], row[1], row[2], row[3], row[4], row[5])
-	return render_template('post.html', post=post)
+    post = []
+    cursor.execute(
+        "SELECT * FROM article WHERE identifiant = (?);", (identifiant,))
+    while True:
+        row = cursor.fetchone()
+        if row is None:
+            break
+        post = (row[0], row[1], row[2], row[3], row[4], row[5])
+    return render_template('post.html', post=post)
+
 
 @app.route("/modifier/<identifiant>", methods=['GET', 'POST'])
 def modifier(identifiant):
-	post=[]
-	cursor.execute("SELECT * FROM article WHERE identifiant = (?);", (identifiant,))
-	while True:
-		row=cursor.fetchone()
-		if row == None:
-			break
-		post= ( row[0], row[1], row[2], row[3], row[4], row[5])
-	form = ModifyPostForm()
-	if form.validate_on_submit():
-		new_titre=form.title.data
-		new_paragraphe=form.content.data
-		id= post[0]
-		cursor.execute("UPDATE article SET titre=?, paragraphe=? WHERE id = (?);", (new_titre, new_paragraphe, id,))
-		flash('Post modifie avec succes!', 'success')
-		return redirect(url_for('admin'))
-	return render_template('modification_post.html', title='Modifier poste', form=form, post=post)
+    post = []
+    cursor.execute(
+        "SELECT * FROM article WHERE identifiant = (?);", (identifiant,))
+    while True:
+        row = cursor.fetchone()
+        if row is None:
+            break
+        post = (row[0], row[1], row[2], row[3], row[4], row[5])
+    form = ModifyPostForm()
+    if form.validate_on_submit():
+        new_titre = form.title.data
+        new_paragraphe = form.content.data
+        id = post[0]
+        cursor.execute(
+            "UPDATE article SET titre=?, paragraphe=? WHERE id = (?);",
+            (new_titre,
+             new_paragraphe,
+             id,
+             ))
+        flash('Post modifie avec succes!', 'success')
+        return redirect(url_for('admin'))
+    return render_template(
+        'modification_post.html',
+        title='Modifier poste',
+        form=form,
+        post=post)
+
 
 @app.route("/search/<recherche>")
 def search(recherche):
-	cursor.execute("SELECT * FROM article WHERE (titre LIKE (?) OR paragraphe LIKE (?));", ("%"+recherche+"%", "%"+recherche+"%"))
-	del posts[:]
-	del postsDup[:]
-	while True:
-		row=cursor.fetchone()
-		if row == None:
-			break
-		post= ( row[0], row[1], row[2], row[3], row[4], row[5])
-		if post not in postsDup:
-			posts.append(post)
-			postsDup.append(post)
-	print(posts)
-	return render_template('accueil.html', posts=posts)
-
+    cursor.execute(
+        "SELECT * FROM article WHERE (titre LIKE (?) OR paragraphe LIKE (?));",
+        ("%" + recherche + "%",
+         "%" + recherche + "%"))
+    del posts[:]
+    del postsDup[:]
+    while True:
+        row = cursor.fetchone()
+        if row is None:
+            break
+        post = (row[0], row[1], row[2], row[3], row[4], row[5])
+        if post not in postsDup:
+            posts.append(post)
+            postsDup.append(post)
+    print(posts)
+    return render_template('accueil.html', posts=posts)
 
 if __name__ == '__main__':
 	app.run(debug=True)
